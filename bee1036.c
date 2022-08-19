71e3e4cf837eff7922f84cd46451a3b93e6ff6cd
omarfaruk-k <ofkhan30@gmail.com>
2022-08-19 10:11:18 +0600

    .

---
+++ bee1036.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    double a,b,c,r1,r2;
+    scanf("%lf %lf %lf",&a,&b,&c);
+    if(a==0 || (b*b<4*a*c)){
+        printf("Impossivel calcular");}
+    else{
+        r1 = (-b+sqrt(b*b-4*a*c))/(2*a);
+        r2 = (-b-sqrt(b*b-4*a*c))/(2*a);
+        printf("R1 = %.5lf\n",r1);
+        printf("R2 = %.5lf",r2);
+    }
+    }
