abcf738db3994bae8549439cb20af082c7e40a6c
omarfaruk-k <ofkhan30@gmail.com>
2023-07-29 23:27:30 +0600

    .

---
+++ A_Gregor_and_Cryptography.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t=0,pr=0;
+    cin>>t;
+
+    while (t--)
+    {
+        int n1=0,n2=0;
+        cin>>pr;
+        n2=pr-1;
+        n1=2;
+        cout<<n1<<" "<<n2<<endl;
+
+    }
+    
+
+    return 0;
+}
--- A_Polycarp_and_Coins.cpp
+++ A_Polycarp_and_Coins.cpp
@@ -1,14 +1,22 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n,big=0,small=0,t;
+    int n,b1=0,b2=0,t;
     cin>>t;
     while(t--){
         cin>>n;
-        big=(n/3);
-        small=big;
+        b1=(n/3);
+        b2=b1;
+
+        if(b1*3!=n){
+            if(n-b1*3==1) b1+=1;
+
+            if(n-b1*3==2) b2+=1;
+        }
+
+
         
-        cout<<small<<" "<<big<<endl;
+        cout<<b1<<" "<<b2<<endl;
     }
     return 0;
 }
