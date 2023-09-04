233aff765a1d0abca5c262d8534a425cb0547f3d
Omar Faruk Khan <98277592+omarfaruk-k@users.noreply.github.com>
2023-09-04 02:33:58 -0700

    Add files via upload

---
+++ LIS2.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+       int n;cin>>n;
+       int list[n];
+
+       for(int i=0;i<n;i++){
+              cin>>list[i];
+       }
+
+       int lis[n];
+       for(int i=0;i<n;i++){
+              lis[i]=1;
+       }
+
+
+
+       for(int i=1;i<n;i++){
+              for(int j=0;j<i;j++){
+                     if(list[j]<list[i]){
+                            lis[i]=max(lis[i],1+lis[j]);
+                     }
+              }
+       }
+
+
+       for(int i=0;i<n;i++){
+              cout<<lis[i]<<" ";
+       }cout<<endl;
+
+       int max=0;
+       for(int i=0;i<n;i++){
+              if(lis[i]>max) max=lis[i];
+       }
+       cout<<max<<endl;
+
+}
+
